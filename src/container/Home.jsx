f0d93b82c7cf7d5ac263b0e1dc73d77fd6e8a7a4
7-18 <kevinbrianbr@gmail.com>
2022-06-28 18:16:29 -0500

    home

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import { HomeDiv, Menu } from "../styles/HomeStyles";
 import { ReactComponent as Bell } from "../assets/images/Notification.svg";
 import { Banners } from "../Components/Banners";
 import { Card } from "../Components/Card";
 import { NavBars } from "../Components/NavBars";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { actionLogoutAsync } from "../redux/actions/actionLogin";
+import { getAuth } from "firebase/auth";
 
 export const Home = () => {
-  const { user } = useSelector((store) => store.registerStore);
+  const dispatch = useDispatch();
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (user) {
+      setProfile(user);
+    }
+  }, []);
 
   return (
     <HomeDiv>
       <Menu>
         <div>
-          <Avatar src={user?.image} alt="nombre" />
+          <Avatar src={profile?.image} alt={profile?.displayName} />
           <div>
             <span>Hi!</span>
-            <span>{user?.name}</span>
+            <span>{profile?.displayName}</span>
           </div>
         </div>
-        <Bell />
+        <Bell onClick={() => dispatch(actionLogoutAsync())} />
       </Menu>
       <Banners />
       <span className="reco">Recommended to you</span>
