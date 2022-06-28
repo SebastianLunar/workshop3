085622bda596100fb350e74cec44804d9414da48
7-18 <kevinbrianbr@gmail.com>
2022-06-28 18:27:08 -0500

    home

--- src/redux/actions/actionRegister.js
+++ src/redux/actions/actionRegister.js
@@ -1,25 +1,55 @@
-import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth"
-import { typesRegister } from "../types/types"
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+} from "firebase/auth";
+import { typesRegister } from "../types/types";
 
-export const actionRegisterAsync = (name, email, phone, country, password, image) => {
-    return (dispatch) => {
-        const auth = getAuth()
-        createUserWithEmailAndPassword(auth, email, password)
-            .then(async ({ user }) => {
-                await updateProfile(auth.currentUser, { displayName: name }, { phoneNumber: phone })
-                dispatch(actionRegisterSync(name, email, phone, country, password, image))
-            })
-            .catch(err => {
-                console.warn(err, 'Usuario No autorizado')
-            })
-    }
-}
+export const actionRegisterAsync = (
+  name,
+  email,
+  phone,
+  country,
+  password,
+  image
+) => {
+  return (dispatch) => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async ({ user }) => {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+          phoneNumber: phone,
+          photoURL:
+            "https://res.cloudinary.com/dd5yolnde/image/upload/v1656300664/buffalo-sprint3/user_fa0maw.png",
+        });
+        dispatch(
+          actionRegisterSync(name, email, phone, country, password, image)
+        );
+      })
+      .catch((err) => {
+        console.warn(err, "Usuario No autorizado");
+      });
+  };
+};
 
-export const actionRegisterSync = (name, email, phone, country, password, image) => {
-    return {
-        type: typesRegister.register,
-        payload: {
-            name, email, phone, country, password, image
-        }
-    }
-}
+export const actionRegisterSync = (
+  name,
+  email,
+  phone,
+  country,
+  password,
+  image
+) => {
+  return {
+    type: typesRegister.register,
+    payload: {
+      name,
+      email,
+      phone,
+      country,
+      password,
+      image,
+    },
+  };
+};
